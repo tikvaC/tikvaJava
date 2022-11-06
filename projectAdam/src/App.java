b87da726fcd0ba0764d28075942a00b68d9cf5a5
avivdan <aviv.danino@boyar.org.il>
2022-11-06 17:48:56 +0200

    hey

--- projectAdam/src/App.java
+++ projectAdam/src/App.java
@@ -1,5 +1,24 @@
+import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("hello tikva");
+        Scanner sc = new Scanner(System.in);
+        
+        int x = sc.nextInt();
+        int max = x;
+        int max2 = x-1;
+        // 1 2 5 4 7 6
+        while (x > -1) {
+            if(max2 < x){
+                if (max < x) {
+                    max2 = max;
+                    max = x;
+                }else if(x != max){
+                    max2 = x;
+                }
+            }
+            x=sc.nextInt();
+        }
+
+        System.out.println("max : " + max);
     }
 }
